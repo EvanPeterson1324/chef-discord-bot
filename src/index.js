c9f2fff27ebbbb6ab416e3b6170d0abac059f2f0
Evan Peterson <evan.peterson216@gmail.com>
2020-07-14 17:46:43 -0400

    fix issue with port binding on heroku

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import { config as importEnvs } from 'dotenv';
 import express from 'express';
 import Discord from 'discord.js';
 import { OK } from 'http-status';
+import { parseMessageAndReply } from './services/botService';
 
 importEnvs();
 
 // Health check http route
 const app = express();
-const PORT = 3000;
+const { PORT } = process.env;
 
 app.get('/', (req, res) => res.status(OK).json({
   message: 'healthy!',
@@ -23,10 +24,8 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.channel.send('pong');
-  }
+client.on('message', message => {
+  parseMessageAndReply(message);
 });
 
 client.login(process.env.DISCORD_TOKEN).catch(e => console.log(e.message));
