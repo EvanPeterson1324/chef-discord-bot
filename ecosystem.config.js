d677076ad3344e1fea80f6ef7c904905f2c7a796
olTWOFACElo <criviecc@gmail.com>
2020-07-15 21:28:33 -0400

    removing deploy object

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,30 +1,15 @@
 module.exports = {
   apps: [
     {
-      script: "dist/index.js",
-      name: "Chef-Bot",
-      instances: "max",
+      script: 'dist/index.js',
+      name: 'Chef-Bot',
       env: {
-        NODE_ENV: "development",
-        watch: "src/**",
+        NODE_ENV: 'development',
+        watch: 'src/**',
       },
       env_production: {
-        NODE_ENV: "production",
+        NODE_ENV: 'production',
       },
     },
   ],
-
-  deploy: {
-    production: {
-      user: "SSH_USERNAME",
-      host: "SSH_HOSTMACHINE",
-      ref: "origin/master",
-      repo: "GIT_REPOSITORY",
-      path: "DESTINATION_PATH",
-      "pre-deploy-local": "",
-      "post-deploy":
-        "npm install && pm2 reload ecosystem.config.js --env production",
-      "pre-setup": "",
-    },
-  },
 };
